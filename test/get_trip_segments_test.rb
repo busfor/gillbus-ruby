f055e2893cfbd39de747d84ab711992e876be9c6
Хребтов Роман <r.hrebtov@busfor.com>
2016-10-28 15:28:38 +0300

    Add method get_trip_segments

---
+++ test/get_trip_segments_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class GetTripSegmentsTest < Minitest::Test
+
+  def get_trip_segments
+    Gillbus::GetTripSegments::Response.parse_string(File.read('test/responses/getTripSegments.xml'))
+  end
+
+  def test_get_trip_segments
+    points = get_trip_segments.points
+
+    assert_equal( 13, points.size )
+    assert_equal( 'Львов', points.first.geography_name )
+    assert_equal( 'Ж/Д вокзал Львів', points.first.name )
+    assert_equal( 'Щецин', points.last.geography_name )
+    assert_equal( 'Автовокзал', points.last.name )
+  end
+end
