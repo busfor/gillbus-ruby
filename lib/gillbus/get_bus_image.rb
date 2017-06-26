ecce5be932b1a97606749668fafb0eb4724e8330
Mike Yurchenkov <mikesehse@gmail.com>
2017-06-26 13:38:37 +0300

    Add request method pick functionality (#13)
    * Add request method pick functionality
    
    * Add new API method support
    
    * Add trailing newline
    
    * bump verstion

---
+++ lib/gillbus/get_bus_image.rb
@@ -0,0 +1,30 @@
+class Gillbus
+  module GetBusImage
+
+    Gillbus.register self, :get_bus_image
+
+    class Request < BaseRequest
+
+      def path; '/online2/GetBusImage' end
+
+      def method; :get end
+
+      # busId
+      # Уникальный ИД заказа, переданный при отложенной продаже.
+      attr_accessor :bus_id
+
+      def params
+        compact(
+          busId: bus_id,
+        )
+      end
+
+    end
+
+    class Response < BaseResponse
+      field :image_urls, [:string], key: 'URL'
+    end
+
+  end
+end
+
