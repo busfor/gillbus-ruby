14dc8d7919170dc9f50ee9767701d10ec7974f12
Mike Yurchenkov <mikesehse@gmail.com>
2017-08-30 14:55:25 +0300

    add one more test case

--- test/search_trips_test.rb
+++ test/search_trips_test.rb
@@ -91,8 +91,8 @@ class SearchTripsResponseTest < Minitest::Test
     assert_equal true, response.trips.first.fake_time_in_road
     assert_equal false, response.trips[1].recommended
     assert_equal true, response.trips.first.recommended
-    assert_equal ['cause 1', 'cause 2'], response.trips.first.tariffs.first.return_cause.map(&:cause)
-    assert_equal [true, false], response.trips.first.tariffs.first.return_cause.map(&:lossless)
+    assert_equal ['cause 1', 'cause 2', 'cause 3'], response.trips.first.tariffs.first.return_cause.map(&:cause)
+    assert_equal [true, false, nil], response.trips.first.tariffs.first.return_cause.map(&:lossless)
     assert_equal ['cause 1'], response.trips[1].tariffs.first.return_cause.map(&:cause)
     assert_equal true, response.trips[1].tariffs.first.is_exclusive_price
   end
