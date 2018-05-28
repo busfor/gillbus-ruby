946506a802ab8a36f7f3443299b684679682c87f
Alexander Sviridov <alexander.sviridov@gmail.com>
2018-05-29 00:28:44 +0300

    Legacy fix

--- lib/gillbus/structs/trip_service.rb
+++ lib/gillbus/structs/trip_service.rb
@@ -15,6 +15,9 @@ class Gillbus
         raise "Bad doc #{doc.inspect}" unless doc.size == 2
         instance.id = doc.first.fetch('ID')
         instance.name = doc.last
+      elsif doc.is_a?(Hash) #legacy data made with MultiXML
+        instance.id = doc.fetch('ID')
+        instance.name = doc.fetch('__content__')
       else
         raise "Bad doc #{doc.inspect}"
       end
